2d0b4a777b03553a205382df42ce6457fd6cbb21
Darshan Hande <darshanhande11@gmail.com>
2021-08-29 14:38:12 +0530

    added Navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import 'semantic-ui-css/semantic.min.css'
 import Home from './Pages/Home/Home'
 import AddCourse from './Pages/Admin/Course/AddCourse/AddCourse'
 import CourseDetail from './Pages/Admin/Course/CourseDetail/CourseDetail'
+import Navbar from './Pages/Shared/Navbar'
 
 const App = ()=> {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/admin/add-course" component={AddCourse} />
-          <Route exact path="/admin/course/:id" component={CourseDetail} />
-        </Switch>
+        <Navbar>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/admin/add-course" component={AddCourse} />
+            <Route exact path="/admin/course/:id" component={CourseDetail} />
+          </Switch>
+        </Navbar>
       </Router>
     </div>
   );
---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { Menu, Icon, Sidebar } from "semantic-ui-react";
+import "./Navbar.css";
+import { createMedia } from "@artsy/fresnel";
+import { Link } from "react-router-dom";
+
+const navStyle = {
+  backgroundColor: "#18535B",
+  borderRadius: "0px",
+};
+
+const AppMedia = createMedia({
+  breakpoints: {
+    mobile: 320,
+    tablet: 768,
+    computer: 992,
+    largeScreen: 1200,
+    widescreen: 1920,
+  },
+});
+
+const { Media } = AppMedia;
+
+const items = [
+  { content: "Home", key: "Home", link: "/" },
+  { content: "About Us", key: "About Us", link: "/about-us" },
+  {
+    content: "Alumni",
+    key: "Alumni",
+    link: "/alumni",
+  },
+  { content: "News", key: "News", link: "/news" },
+  { content: "Events", key: "Events", link: "/events" },
+  { content: "Blogs", key: "Blogs", link: "/blogs" },
+];
+
+const NavBarMobile = (props) => {
+  const [visible, setVisible] = useState(false);
+  const pushHandler = () => {
+    if (visible) setVisible(false);
+  };
+  const [activeItem, setActiveItem] = useState("");
+  const clickHandler = (item) => {
+    setActiveItem(item.content);
+    setVisible(false);
+  };
+  const toggleHandler = () => setVisible(!visible);
+  return (
+    <>
+      <Menu inverted style={navStyle} widths={4}>
+        <Menu.Item onClick={toggleHandler}>
+          <Icon name="sidebar" />
+        </Menu.Item>
+      </Menu>
+      <Sidebar.Pushable className="side-push">
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          direction="top"
+          icon="labeled"
+          inverted
+          style={navStyle}
+          vertical
+          visible={visible}
+        >
+          {items.map((item) => (
+            <Menu.Item
+              {...item}
+              name={item}
+              active={activeItem === item.content}
+              onClick={() => clickHandler(item)}
+              className="nav-items"
+              as={Link}
+              to={item.link}
+            ></Menu.Item>
+          ))}
+        </Sidebar>
+        <Sidebar.Pusher
+          dimmed={visible}
+          onClick={pushHandler}
+          style={{ minHeight: "100vh" }}
+          className="side-push"
+        >
+          {props.children}
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    </>
+  );
+};
+
+const NavBarDesktop = (props) => {
+  const [activeItem, setActiveItem] = useState("");
+  const clickHandler = (item) => {
+    setActiveItem(item.content);
+  };
+  return (
+    <>
+      <Menu inverted widths={9} style={navStyle}>
+        {items.map((item) => (
+          <Menu.Item
+            {...item}
+            name={item}
+            active={activeItem === item.content}
+            onClick={() => clickHandler(item)}
+            className="nav-items"
+            as={Link}
+            to={item.link}
+          ></Menu.Item>
+        ))}
+      </Menu>
+      {props.children}
+    </>
+  );
+};
+
+const Navbar = (props) => {
+  return (
+    <div>
+      <Media at="mobile">
+        <NavBarMobile>{props.children}</NavBarMobile>
+      </Media>
+
+      <Media greaterThan="mobile">
+        <NavBarDesktop>{props.children}</NavBarDesktop>
+      </Media>
+    </div>
+  );
+};
+
+export default Navbar;
