1952faccdf844911aa25cd2624afad552f60db84
Ayush Patel <in.ayushpatel@gmail.com>
2021-09-24 16:10:07 +0530

    rolelist+addrole / not delete

--- src/Pages/Admin/AccessRoles/AddRole.jsx
+++ src/Pages/Admin/AccessRoles/AddRole.jsx
@@ -1,15 +1,73 @@
-import React from 'react';
+import { useState, useEffect , React} from 'react';
 import {  Message } from 'semantic-ui-react';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import axios from 'axios';
+import { Dropdown } from 'semantic-ui-react'
 
 
 const AddRole = ()=>{
 
-    const state = {
-        name : "",
-        email : "",
-        role : "",
+
+    const behost = process.env.REACT_APP_BACKEND_HOST;
+    const [data, setData] = useState({});
+    const [redirect, setRedirect] = useState(null);
+
+    const addrole = async (e) => {
+      e.preventDefault();
+      console.log(data);
+        try {
+          if(!data.name||!data.email||!data.role){
+            return;
+          }
+          await axios({
+            method: "POST",
+            url: behost + "access/addrole",
+            data,
+            withCredentials: true
+          })
+          setRedirect("/admin/rolelist")
+        } catch (error) {
+          console.log(error.message);
+        }
+    }
+
+    useEffect(()=>{
+        axios.get(`${behost}auth/status`).then((res) => {
+          if (!res.data.user || res.data.user.role!=="admin") {
+             if(!res.data.user){
+               setRedirect("/");
+             }else if(res.data.user==="istructor"){
+               setRedirect("/instructor/dashboard")
+             }else{
+               setRedirect("/student/dashboard")
+             }
+          }
+        })
+      },[])
+
+    const setInfo = (e) => {
+      setData({
+        ...data,
+        [e.target.name]: e.target.value
+      })
+    }
+
+    const setInfoDropdown = (e,{ name, value }) => {
+      console.log(value);
+      setData({
+        ...data,
+        [name]: value,
+      });
     };
+
+    if (redirect) {
+      return <Redirect to={redirect} />
+    }
+    
+    const roles = [
+      { key : 1, text: 'Admin', value: "admin"},
+      { key: 2, text: 'Instructor', value: "instructor"}
+    ]
 
     return(
         <div className = "ui container">
@@ -18,20 +76,30 @@ const AddRole = ()=>{
             <form className = "ui form" >
                 <div className = "field">
                     <label> Name </label>
-                    <input type = "text" name = "name" placeholder = "Name"/>
+                    <input type = "text" name = "name" placeholder = "Name" onChange={(e) => setInfo(e)}/>
                 </div>
                 <div className = "field">
                     <label> Email</label>
-                    <input type = "text" name = "email" placeholder = "E-mail"/>
+                    <input type = "text" name = "email" placeholder = "E-mail" onChange={(e) => setInfo(e)}/>
                 </div>
                 <div className = "field">
                     <label>Role</label>
-                    <select className = "ui fluid dropdown" >
-                        <option value = "Admin">Admin</option>
-                        <option value = "Instructor">Instructor</option>
-                    </select>
+                    <Dropdown
+                      placeholder='Select Role'
+                      selection
+                      button
+                      name="role"
+                      header= 'role'
+                      options={roles}
+                      onChange={setInfoDropdown}
+                      required
+                    />
+                    {/* <select className = "ui fluid dropdown" name ="role" onChange={setInfoDropdown} >
+                        <option value = "admin">Admin</option>
+                        <option value = "instructor">Instructor</option>
+                    </select> */}
                 </div>
-                <button className = "ui button blue">Add</button>
+                <button className = "ui button blue" onClick={addrole}>Add</button>
             </form>
             <Message>
                <strong>View <Link to="/admin/rolelist">roleList</Link></strong>
--- src/Pages/Admin/AccessRoles/RoleList.jsx
+++ src/Pages/Admin/AccessRoles/RoleList.jsx
@@ -1,31 +1,61 @@
-import React from 'react';
 import RoleCard from '../AccessRoles/RoleCard';
-import { Link } from "react-router-dom";
+import { useState, useEffect, React } from 'react';
+import { Redirect,useParams,Link } from 'react-router-dom'
 import { Table,Button,Message } from 'semantic-ui-react'
+import axios from 'axios';
+
 
 const RoleList = () => {
-    let data = [
-        {
-            name: "Keshav Agarwal",
-            email: "keshavagarwal@gmail.com",
-            role: "admin"
-        },
-        {
-            name: "Abhay",
-            email: "abhay@gmail.com",
-            role: "admin"
-        },
-        {
-            name: "Ayush",
-            email: "ayush@gmail.com",
-            role: "instructor"
-        },
-        {
-            name: "Darshan",
-            email: "darshan@gmail.com",
-            role: "instructor"
+
+
+    const [data,setData] = useState([]);
+    const [redirect, setRedirect] = useState(null);
+    const behost = process.env.REACT_APP_BACKEND_HOST;
+    const [loading,setLoading] = useState(true);
+    
+    const fetchData = async()=>{
+        try {
+            let res = await axios.get(`${behost}access/getroles`);
+            if(res.data.length>0){
+                setData(res.data);
+                setLoading(false);
+            }else{
+                setRedirect("/404")
+            }
+        } catch (error) {
+            console.log(error.message)
         }
-    ]
+    }
+
+    const handleDelete = async(e, id)=>{
+        console.log(id);
+    //    try {
+    //     await axios.delete(`${behost}access/delete/${id}`)
+    //     setRedirect("/admin/rolelist")
+    //    } catch (error) {
+    //        console.log(error.message);
+    //    }
+    }
+
+    useEffect(()=>{
+        axios.get(`${behost}auth/status`).then((res) => {
+          if (!res.data.user || res.data.user.role!=="admin") {
+             if(!res.data.user){
+               setRedirect("/");
+             }else if(res.data.user==="istructor"){
+               setRedirect("/instructor/dashboard")
+             }else{
+               setRedirect("/student/dashboard")
+             }
+          }else{
+              fetchData();
+          }
+        })
+      },[])
+    
+    if (redirect) {
+        return <Redirect to={redirect} />
+    }
     
     return (
         <div className="ui container">
@@ -38,7 +68,7 @@ const RoleList = () => {
                             <Table.HeaderCell>Name</Table.HeaderCell>
                             <Table.HeaderCell>Email</Table.HeaderCell>
                             <Table.HeaderCell>Role</Table.HeaderCell>
-                            <Table.HeaderCell>Edit</Table.HeaderCell>
+                            <Table.HeaderCell>Delete</Table.HeaderCell>
                         </Table.Row>
                     </Table.Header>
                     <Table.Body>
@@ -48,7 +78,7 @@ const RoleList = () => {
                                 <Table.Cell>{val.name}</Table.Cell>
                                 <Table.Cell>{val.email}</Table.Cell>
                                 <Table.Cell>{val.role}</Table.Cell>
-                                <Table.Cell><Button>Edit</Button></Table.Cell>
+                                <Table.Cell><Button onClick={handleDelete(val.id)} >Delete</Button></Table.Cell>
                             </Table.Row>
                         ))}
                     </Table.Body>
